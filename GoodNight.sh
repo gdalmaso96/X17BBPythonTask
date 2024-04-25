e6ce61314771c3d1f72b47623127633bbe12d9a6
gdalmaso96 <g.dalmaso@outlook.it>
2024-04-25 11:29:01 +0200

    Run locally FC generation

---
+++ GoodNight.sh
@@ -0,0 +1,36 @@
+#python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED   0 --nToys 200
+#python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED   0 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED   0 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED   0 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED   0 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED   0 --nToys 200
+python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED 200 --nToys 200
+python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED 400 --nToys 200
+python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED 600 --nToys 200
+python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED 800 --nToys 200
+python3 generateFCtest_2023.py --SignalYield   0 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
+python3 generateFCtest_2023.py --SignalYield  50 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
+python3 generateFCtest_2023.py --SignalYield 100 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
+python3 generateFCtest_2023.py --SignalYield 150 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
+python3 generateFCtest_2023.py --SignalYield 200 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
+python3 generateFCtest_2023.py --SignalYield 250 --SignalMass 16.9 --SEED   0 --nToys 100 --generateDataSets True --DataSEED 0 --DataSignalMass 16.9 --DataSignalYield 0
